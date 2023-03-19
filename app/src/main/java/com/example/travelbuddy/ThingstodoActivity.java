46d8b258006821edbdc0758d091f73d47f2b63b1
sarveshtiku <64361696+sarveshtiku@users.noreply.github.com>
2023-03-18 21:28:04 -0600

    added list

--- app/src/main/java/com/example/travelbuddy/ThingstodoActivity.java
+++ app/src/main/java/com/example/travelbuddy/ThingstodoActivity.java
@@ -75,7 +75,9 @@ public class ThingstodoActivity extends AppCompatActivity {
         } catch (NullPointerException e) {
             Log.e("TAG", "Failed to load meta-data, NullPointer: " + e.getMessage());
         }
-        List<String> categories = Arrays.asList("Parks", "Gyms", "Art", "Attractions");
+        List<String> categories = Arrays.asList(
+                "accounting", "airport", "amusement_park", "aquarium", "art_gallery", "atm", "bakery", "bank", "bar", "beauty_salon", "bicycle_store", "book_store", "bowling_alley", "bus_station", "cafe", "campground", "car_dealer", "car_rental", "car_repair", "car_wash", "casino", "cemetery", "church", "city_hall", "clothing_store", "convenience_store", "courthouse", "dentist", "department_store", "doctor", "drugstore", "electrician", "electronics_store", "embassy", "fire_station", "florist", "funeral_home", "furniture_store", "gas_station", "gym", "hair_care", "hardware_store", "hindu_temple", "home_goods_store", "hospital", "insurance_agency", "jewelry_store", "laundry", "lawyer", "library", "light_rail_station", "liquor_store", "local_government_office", "locksmith", "lodging", "meal_delivery", "meal_takeaway", "mosque", "movie_rental", "movie_theater", "moving_company", "museum", "night_club", "painter", "park", "parking", "pet_store", "pharmacy", "physiotherapist", "plumber", "police", "post_office", "primary_school", "real_estate_agency", "restaurant", "roofing_contractor", "rv_park", "school", "secondary_school", "shoe_store", "shopping_mall", "spa", "stadium", "storage", "store", "subway_station", "supermarket", "synagogue", "taxi_stand", "tourist_attraction", "train_station", "transit_station", "travel_agency", "university", "veterinary_care", "zoo"
+        );
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -84,8 +86,7 @@ public class ThingstodoActivity extends AppCompatActivity {
         ThingsToDoCategoryAdapter adapter = new ThingsToDoCategoryAdapter(categories, new ThingsToDoCategoryAdapter.OnCategoryClickListener() {
             @Override
             public void onCategoryClick(int position) {
-                // Handle click event here
-            }
+                            }
         });
         recyclerView.setAdapter(adapter);}
 
